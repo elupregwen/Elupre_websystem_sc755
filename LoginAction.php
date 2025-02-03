a3e3180c8ac3d053e6e204e133ef8948ba0400ae
elupregwen <elupreshanicagwen@gmail.com>
2025-02-02 17:54:05 -0800

    gwen
    commit

--- LoginAction.php
+++ LoginAction.php
@@ -1,36 +1,38 @@
-<!-- Comment -->
 <?php
 session_start();
-
 include 'database.php';
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $stmt -> $conn -> prepare("SELECT id, userid, name, email, password FROM users WHERE email = ?");
-    $stmt -> bind_param("s", $email);
-    $stmt -> execute();
-    $result -> stmt -> get_result();
+   
+    $stmt = $conn->prepare("SELECT id, userid, name, email, password FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    if($result -> num_rows > 0){
-        $row = $result -> fetch_assoc();
+   
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
 
-        if($password ==$row["password"]){
+     
+        if ($password === $row['password']) {
+           
             $_SESSION['user'] = $row['name'];
 
-            header("Location", "dashboard.php");
+         
+            header("Location: dashboard.php");
             exit();
-
-        }else{
-            $error = "Invalid email or password";
+        } else {
+            $error = "Invalid email or password!";
         }
-    }else{
-        $error = "Invalid email or password";
+    } else {
+        $error = "Invalid email or password!";
     }
-
-    $stmt -> close();
-    $conn -> close()
+   
+   
+    $stmt->close();
+    $conn->close();
 }
-
 ?>
